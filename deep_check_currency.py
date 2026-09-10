888a886ee7692e635279752e21400f3e5d8ed5c9
Dokets <ashokdokets@gmail.com>
2026-09-11 04:04:15 +0530

    Move dev scripts to scripts/, cleanup root folder

--- deep_check_currency.py
+++
@@ -1,8 +0,0 @@
-﻿with open('static/js/currency-utils.js', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-print(f'Total lines: {len(lines)}')
-print(f'\n=== Lines 125-140 ===')
-for i in range(124, min(140, len(lines))):
-    marker = ' ← ERROR' if i == 133 else ''
-    print(f'{i+1}: {lines[i].rstrip()[:120]}{marker}')
--- show_login.py
+++
@@ -1,7 +0,0 @@
-﻿with open('templates/login.html', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-import re
-match = re.search(r'async function loginUser.*?\n\s*\}', content, re.DOTALL)
-if match:
-    print(match.group())
